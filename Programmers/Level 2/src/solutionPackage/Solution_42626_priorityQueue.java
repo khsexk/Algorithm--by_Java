23173d8bf60637cddbfa3d748ca7ff330bcbed8c
khsexk <kohyunsuk98@gmail.com>
2021-07-27 14:18:01 +0900

    42626 correct Solution

---
+++ Programmers/Level 2/src/solutionPackage/Solution_42626_priorityQueue.java
@@ -0,0 +1,29 @@
+package solutionPackage;
+import java.util.*;
+
+public class Solution_42626_priorityQueue {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> scv = new PriorityQueue<Integer>();
+        
+        for(int i : scoville)
+            scv.offer(i);
+        
+        int answer = 0;
+        int min = 0;
+        
+        while(true){
+            min = (int)scv.peek();
+            if(min>=K)
+                break;
+            if(scv.size() == 1)
+                break;
+            int newFood = (int)scv.poll() + (int)scv.poll()*2;
+            scv.offer(newFood);
+            answer++;
+        }
+        if(min<K)
+            answer = -1;
+        
+        return answer;
+    }
+}
