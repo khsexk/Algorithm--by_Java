35935c08937b099f034aa1cdcdb6bf7fd7773a24
khsexk <kohyunsuk98@gmail.com>
2021-08-01 00:21:57 +0900

    12910 나누어 떨어지는 숫자 배열 Solution

---
+++ Programmers/Level 1/src/solutionPackage/Solution_12910.java
@@ -0,0 +1,26 @@
+package solutionPackage;
+import java.util.ArrayList;
+
+public class Solution_12910 {
+    public int[] solution(int[] arr, int divisor) {
+        ArrayList<Integer> arrList = new ArrayList();
+		
+		for(int i=0 ; i<arr.length ; i++) {
+			if(arr[i]%divisor == 0)
+				arrList.add(arr[i]);
+		}
+		
+		if(arrList.size()==0) {
+			int[] answer = {-1};
+			return answer;
+		}
+		
+		int[] answer = new int[arrList.size()];
+		
+		for(int j=0 ; j<arrList.size(); j++) 
+			answer[j] = arrList.get(j);
+		Arrays.sort(answer);
+		
+        return answer;
+    }
+}
