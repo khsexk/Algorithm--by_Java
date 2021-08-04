6199a88344309b4e58dce9d1d062035f8c0abfcf
khsexk <kohyunsuk98@gmail.com>
2021-08-04 22:16:49 +0900

    12925 문자열을 정수로 바꾸기 Solution

---
+++ Programmers/Level 1/src/solutionPackage/Solution_12925.java
@@ -0,0 +1,9 @@
+package solutionPackage;
+
+public class Solution_12925 {
+    public int solution(String s) {
+        Integer i = new Integer(s);
+        int answer = i;
+        return answer;
+    }
+}
