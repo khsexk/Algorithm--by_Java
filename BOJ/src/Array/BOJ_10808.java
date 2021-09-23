4be672cd7ed3a4f7971b0207b565901ca46b2d8f
khsexk <kohyunsuk98@gmail.com>
2021-09-23 22:49:20 +0900

    BOJ 문제풀이 Project 생성

---
+++ BOJ/src/Array/BOJ_10808.java
@@ -0,0 +1,16 @@
+package Array;
+import java.util.*;
+public class BOJ_10808 {
+
+	public static void main(String[] args) {
+		char[] c = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
+				'n','o','p','q','r','s','t','u','v','w','y','z'};
+		int[] n = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		
+		for(int i=0)
+		
+	}
+
+}
