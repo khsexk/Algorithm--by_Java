149a036d02a9feee24fe7f23e1bcae862f473818
khsexk <kohyunsuk98@gmail.com>
2021-11-08 00:23:45 +0900

    1158 요세푸스 문제 Solution

---
+++ BOJ/src/LinkedList/BOJ_1158.java
@@ -0,0 +1,33 @@
+package LinkedList;
+import java.util.*;
+
+public class BOJ_1158 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		StringBuilder sb = new StringBuilder("<");
+		
+		for(int i=1 ; i<=N ; i++) 
+			list.add(i);
+		
+		while(!list.isEmpty()) {
+			for(int i=0 ; i<K ; i++) {
+				if(i==K-1)
+					sb.append(list.poll());
+				else {
+					list.offer(list.poll());
+				}
+			}
+			if(!list.isEmpty()) {
+				sb.append(", ");
+			}
+		}
+		
+		System.out.println(sb.append(">").toString());
+	}
+
+}
