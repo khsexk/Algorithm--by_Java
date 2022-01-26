625ca554d4e34986346ba8f13135009a08450de4
khsexk <kohyunsuk98@gmail.com>
2022-01-27 02:44:58 +0900

    BOJ 2457 공주님의 정원

---
+++ BOJ/src/Greedy/BOJ_2457.java
@@ -0,0 +1,77 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class BOJ_2457 {
+	static final int start = 301;
+	static final int finish = 1201;
+	static int current = 301;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		ArrayList<Flower> flowerList = new ArrayList<>();
+		int idx = 0;
+		
+		for(int i=0 ; i<N ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			flowerList.add(new Flower(Integer.valueOf(st.nextToken())*100 + Integer.valueOf(st.nextToken()),
+									Integer.valueOf(st.nextToken())*100 + Integer.valueOf(st.nextToken())));
+		}
+		
+		Collections.sort(flowerList);
+		
+		int minFlower = 0;
+		boolean flag = false;
+		
+		while(current < finish) {
+			int maxfinishTime = current;
+			
+			for(Flower flower : flowerList) {
+				if(flower.startDate <= current && maxfinishTime < flower.finishDate) {
+					maxfinishTime = flower.finishDate;
+					flag = true;
+				}
+			} // for_Each
+			
+			if(flag) {
+				current = maxfinishTime;
+				minFlower++;
+				flag = false;
+			}
+			else {
+				minFlower = 0;
+				break;
+			}
+		} // while
+		bw.write(Integer.toString(minFlower));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
+class Flower implements Comparable<Flower>{
+	int startDate;
+	int finishDate;
+	
+	Flower(int startDate, int finishDate){
+		this.startDate = startDate;
+		this.finishDate = finishDate;
+	}
+
+	@Override
+	public int compareTo(Flower f2) {
+		if(this.startDate == f2.startDate) {
+			return this.finishDate - f2.finishDate;
+		}
+		return this.startDate - f2.startDate;
+	}
+}
