a67cecaf24a8e4fd8921a33ce77431c8857b69b7
khsexk <kohyunsuk98@gmail.com>
2022-02-14 00:57:08 +0900

    BOJ 1260 DFS와 BFS

--- BOJ/src/DFS_BFS/BOJ_1260.java
+++ BOJ/src/DFS_BFS/BOJ_1260.java
@@ -1,5 +1,8 @@
 package DFS_BFS;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -7,86 +10,86 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
-class DfsAndBfs1260 { 
-	static List<Integer>[] arr; // 인접 리스트 표현 
-	static List<Integer> dfsResult; // dfs 결과 
-	static List<Integer> bfsResult; // bfs 결과 
-	static boolean[] check; // 방문여부 체크 
-	public static void main(String[] args) { 
-		// === 입력 === 
-		Scanner sc = new Scanner(System.in); 
-		int n = sc.nextInt(); 
-		int len = sc.nextInt(); 
-		int start = sc.nextInt(); 
-		arr = new List[n + 1]; 
+public class BOJ_1260 { 
+	static StringTokenizer st;
+	static ArrayList<Integer>[] arr; 
+	static ArrayList<Integer> dfsResult; 
+	static ArrayList<Integer> bfsResult; // bfs 결과 
+	static boolean[] visited; // 방문여부 체크 
+	
+	public static void main(String[] args) throws IOException{ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine(), " ");
 		
-		for(int i = 0; i <= n; i++) 
+		int N = Integer.parseInt(st.nextToken()); 
+		int M = Integer.parseInt(st.nextToken()); 
+		int V = Integer.parseInt(st.nextToken()); 
+		arr = new ArrayList[N+1]; 
+		
+		for(int i = 0; i <= N; i++) 
 			arr[i] = new ArrayList<>(); 
 		
-		for(int i = 0; i < len; i++) { 
-			int n1 = sc.nextInt(); 
-			int n2 = sc.nextInt(); // 양방향 모두 표시 
-			arr[n1].add(n2); 
-			arr[n2].add(n1); 
-			} 
-		sc.close(); 
-		// === 구현 === 
-		// 인접 리스트 정렬 ( 작은수부터 방문하기 위해 ) 
+		for(int i = 0; i < M; i++) { 
+			st = new StringTokenizer(br.readLine(), " ");
+			int node1 = Integer.parseInt(st.nextToken()); 
+			int node2 = Integer.parseInt(st.nextToken()); 
+			arr[node1].add(node2); 
+			arr[node2].add(node1); 
+		} 
+		
 		for(List<Integer> node : arr) 
 			Collections.sort(node); 
+		
 		dfsResult = new ArrayList<>(); 
 		bfsResult = new ArrayList<>(); 
-		check = new boolean[n + 1]; 
-		dfs(start); 
-		// dfs 시작점 부터 
-		Arrays.fill(check, false); 
-		// check 전부 false로 초기화 
-		bfs(start); 
-		//bfs 시작점 부터 
+		visited = new boolean[N + 1]; 
+		
+		dfs(V); 
+		Arrays.fill(visited, false); 
+		bfs(V); 
+		
 		// === 결과 출력 === 
 		for(int i = 0; i < dfsResult.size(); i++){ 
 			System.out.print(dfsResult.get(i) + " "); 
-			} 
+		} 
 		System.out.println(); 
+		
 		for(int i = 0; i < bfsResult.size(); i++){ 
 			System.out.print(bfsResult.get(i) + " "); 
-			} 
-		} // 깊이우선 
-	public static void dfs(int idx){ 
-		if(check[idx]) return; 
-		check[idx] = true; 
-		// 현재 위치 방문처리 
-		dfsResult.add(idx); 
-		// 결과에 현재 위치 추가 
-		// 현재 노드에서 방문 가능한 노드중에 아직 방문 안한 노드 재귀호출 
-		for(int x : arr[idx]){ 
-			if(!check[x]) dfs(x); 
-			} 
 		} 
-	// 너비우선 
-	public static void bfs(int start){ 
+	}
+	
+	public static void dfs(int node){ 
+		if(visited[node]) return;
+		
+		visited[node] = true; 
+		dfsResult.add(node); 
+
+		for(int x : arr[node]){ 
+			if(!visited[x]) 
+				dfs(x); 
+		} 
+	} 
+	
+	public static void bfs(int root){ 
 		Queue<Integer> q = new LinkedList<>(); 
-		// 방문할 노드를 저장하는 용도 
-		q.offer(start); 
-		// 처음 값 추가 
-		check[start] = true; 
-		// 처음 값 방문처리 
+
+		q.offer(root); 
+		visited[root] = true; 
+
 		while(!q.isEmpty()){ 
-			int x = q.poll(); 
-			// 먼저 삽입된 값 
-			bfsResult.add(x); 
-			// 결과 추가 
-			// 현재 노드에서 방문 가능한 노드중에 아직 방문 안한 노드 체크하며 큐에 추가 
-			for(int y : arr[x]){ 
-				if(!check[y]) { 
-					check[y] = true; q.add(y); 
-					} 
+			int v = q.poll(); 
+
+			bfsResult.add(v); 
+
+			for(int y : arr[v]){ 
+				if(!visited[y]) { 
+					visited[y] = true; 
+					q.add(y); 
 				} 
 			} 
 		} 
-	}
-		}
-	}
+	} 
 }
-
